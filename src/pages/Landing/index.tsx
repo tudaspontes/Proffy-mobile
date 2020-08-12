ac58178ab06e3dc78428a40cf5cafefdbb7d87cd
Felipe <felipedemelopontes@gmail.com>
2020-08-12 21:40:19 +0200

    feat: frontend done

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -18,6 +18,10 @@ function Landing() {
         navigate('GiveClasses');
     }
 
+    function handleNavigateToStudyPage() {
+        navigate('Study');
+    }
+
     return (
         <View style={styles.container} >
             <Image source={landingImg} style={styles.banner} />
@@ -27,7 +31,9 @@ function Landing() {
                 <Text style={styles.titleBold}>Escolha o que deseja fazer</Text>
             </Text>
             <View style={styles.buttonsContainer}>
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton
+                    onPress={handleNavigateToStudyPage}
+                    style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon} />
 
                     <Text style={styles.buttonText}>Estudar</Text>
