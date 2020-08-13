97e35f1bddaddd545e2d5ed1282edf0b30bf58cb
Felipe <felipedemelopontes@gmail.com>
2020-08-13 23:42:19 +0200

    feat: filters

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { View, Image, Text } from "react-native";
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './styles';
+
 
 function TeacherItem() {
     return (
@@ -20,6 +26,25 @@ function TeacherItem() {
             <Text style={styles.bio}>
             Front-end Developer | HTML, CSS and JavaScript Developer | Self-taught Programmer | Polyglot
             </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                Preço/hora: {'   '}
+                <Text style={styles.priceValue}>R$ 20,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
+                        <Image source={unfavoriteIcon} />
+                        {/* <Image source={heartOutlineIcon} /> */}
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}> Entrar em contato </Text>
+                    </RectButton>
+                </View>
+            </View>
         </View>
     )
 }
--- src/components/TeacherItem/styles.ts
+++ src/components/TeacherItem/styles.ts
@@ -47,7 +47,63 @@ const styles = StyleSheet.create({
         fontSize: 14,
         lineHeight: 24,
         color: '#6a6180'
-    }
+    },
+
+    footer: {
+        backgroundColor: '#fafafc',
+        padding: 24,
+        alignItems: 'center',
+        marginTop: 24,
+    },
+
+    price: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#6a6180',
+        fontSize: 14,
+    },
+
+    priceValue: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#8247e5',
+        fontSize: 16,
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 16,
+    },
+
+    favoriteButton: {
+        backgroundColor: '#8257e5',
+        width: 56,
+        height: 56,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8,
+    },
+
+    favorited: {
+        backgroundColor: '#e33d3d'
+    },
+
+    contactButton: {
+        backgroundColor: '#04d361',
+        flex: 1,
+        height: 56,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8,
+    },
+
+    contactButtonText: {
+        color: '#FFF',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 16,
+        marginLeft: 16
+    },
 })
 
 export default styles;
