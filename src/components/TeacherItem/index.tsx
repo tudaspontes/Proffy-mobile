d3dfbe8b51992e7220c153b5f61682df4e7c95d5
Felipe <felipedemelopontes@gmail.com>
2020-08-14 17:42:43 +0200

    feat: app done.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,45 +1,103 @@
-import React from 'react';
-import { View, Image, Text } from "react-native";
+import React, { useState } from 'react';
+import { View, Image, Text, Linking } from "react-native";
 import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage'
+
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
 import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './styles';
+import api from '../../services/api';
 
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
 
-function TeacherItem() {
+interface TeacherItemProps {
+    teacher: Teacher;
+    favorited: boolean;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+    const [isFavorited, setIsFavorited] = useState(favorited);
+    function handleLinktoWhatsapp() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+        
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+    }
+
+    async function handleToggleFavorite(){
+        const favorites = await AsyncStorage.getItem('favorites')
+
+        let favoritesArray = [];
+
+        if (favorites) {
+            favoritesArray = JSON.parse(favorites);
+        }
+
+        if (isFavorited) {
+            const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+                return teacherItem.id === teacher.id;
+            })
+
+            favoritesArray.splice(favoriteIndex, 1);
+            setIsFavorited(false);
+        } else {
+            favoritesArray.push(teacher);
+
+            setIsFavorited(true);
+        }
+
+        await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
+    }
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <Image
                     style={styles.avatar}
-                    source={{ uri: 'https://avatars0.githubusercontent.com/u/53872262?s=460&u=afb37d9f31319203c5161593a2c41edb970f9387&v=4' }}
+                    source={{ uri: teacher.avatar }}
                 />
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Felipe Pontes</Text>
-                    <Text style={styles.subject}>Matematica</Text>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
                 </View>
             </View>
 
             <Text style={styles.bio}>
-            Front-end Developer | HTML, CSS and JavaScript Developer | Self-taught Programmer | Polyglot
+            {teacher.bio}
             </Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
-                Preço/hora: {'   '}
+                Preço/hora: {teacher.cost}
                 <Text style={styles.priceValue}>R$ 20,00</Text>
                 </Text>
 
                 <View style={styles.buttonsContainer}>
-                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
-                        <Image source={unfavoriteIcon} />
-                        {/* <Image source={heartOutlineIcon} /> */}
+                    <RectButton
+                        onPress={handleToggleFavorite}
+                        style={[
+                            styles.favoriteButton,
+                            isFavorited ? styles.favorited : {},
+                            ]}
+                        >
+                        { isFavorited 
+                            ? <Image source={unfavoriteIcon} />
+                            : <Image source={heartOutlineIcon} />
+                    }
                     </RectButton>
 
-                    <RectButton style={styles.contactButton}>
+                    <RectButton onPress={handleLinktoWhatsapp} style={styles.contactButton}>
                         <Image source={whatsappIcon} />
                         <Text style={styles.contactButtonText}> Entrar em contato </Text>
                     </RectButton>
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native'
 
 import styles from './styles';
+
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
+import AsyncStorage from '@react-native-community/async-storage';
+
 
 function Favorites() {
+    const [favorites,setFavorites] = useState([]);
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+                const favoritedTeachers = JSON.parse(response);
+
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+
     return (
         <View style={styles.container}>
             <PageHeader title="Meus Proffys favoritos" />
@@ -16,12 +36,15 @@ function Favorites() {
             paddingBottom: 16,
         }}
         >
-
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+           {favorites.map((teacher: Teacher) => {
+               return (
+                <TeacherItem
+                    key={teacher.id}
+                    teacher={teacher}
+                    favorited                
+                />
+               )
+           })}
     </ScrollView>
         </View>
     )
